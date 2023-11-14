2222181ed8eb533c2197976d4534a357972ee1eb
rileytittle <coletittle@ymail.com>
2023-11-13 19:22:56 -0500

    Create Node class and Project4 class

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,23 @@
 
 public class Node {
-
+	private String countryName;
+	private double happinessIndex;
+	private Node leftChild;
+	private Node rightChild;
+	
+	public Node(String countryName, double happinessIndex){
+		this.countryName = countryName;
+		this.happinessIndex = happinessIndex;
+		leftChild = null;
+		rightChild = null;
+	}
+	
+	public void print(){
+		//System.out.println(this.name + "\t" + this.capital + "\t" + this.Gdp + "\t" + this.area + "\t" + this.happyIndex);
+		//String test = "Cole";
+		//String tset = "Tittle";
+		//System.out.printf("%s%s", test, tset);
+		System.out.printf("%-38s%-18.3f", countryName, happinessIndex);
+		System.out.println();
+	}//end print method
 }
---
+++ src/Project4.java
@@ -0,0 +1,10 @@
+
+public class Project4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node node = new Node("Democratic Republic of the Congo", 3.207);
+		node.print();
+	}
+
+}
