ec237c26dae6f400070f604530d4ee12a1530eff
rileytittle <coletittle@ymail.com>
2023-11-16 02:07:51 -0500

    Lots of changes

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,7 +1,7 @@
+import java.util.NoSuchElementException;
 
 public class BinarySearchTree {
 	private Node root;
-	
 	public BinarySearchTree(){
 		root = null;
 	}
@@ -16,6 +16,10 @@ public class BinarySearchTree {
 			Node parent;
 			while(true){
 				parent = current;
+				if(newNode.getName().compareToIgnoreCase(current.getName()) == 0){
+					System.out.println("Country already exists in tree");
+					return;
+				}
 				if(newNode.getName().compareTo(current.getName()) < 0){
 					current = current.leftChild;
 					if(current == null){
@@ -51,8 +55,105 @@ public class BinarySearchTree {
 				return -1;
 			}
 		}//end else
+		System.out.println(name + " is found with happiness of " + current.getHappiness());
 		System.out.println("Path to " + name + " is " + path + name);
 		return current.getHappiness();
+	}
+	//I think this works at the moment. Ran with some test code and it
+	//performed fine
+	public void delete(String name){
+		Node current = root;
+		Node parent = root;
+		boolean isLeftChild = true;
+		while(current.getName().compareTo(name) != 0){
+			parent = current;
+			if(current.getName().compareTo(name) > 0){
+				isLeftChild = true;
+				current = current.leftChild;
+			}
+			else{
+				isLeftChild = false;
+				current = current.rightChild;
+			}
+			if(current == null){
+				return;
+			}
+		}
+		if(current.leftChild == null && current.rightChild == null){
+			if(current == root){
+				root = null;
+			}
+			else if(isLeftChild){
+				parent.leftChild = null;
+			}
+			else{
+				parent.rightChild = null;
+			}
+		}
+		else if(current.rightChild == null){
+			if(current == root){
+				root = current.leftChild;
+			}
+			else if(isLeftChild){
+				parent.leftChild = current.leftChild;
+			}
+			else{
+				parent.rightChild = current.leftChild;
+			}
+		}
+		else if(current.leftChild == null){
+			if(current == root){
+				root = current.rightChild;
+			}
+			else if(isLeftChild){
+				parent.leftChild = current.rightChild;
+			}
+			else{
+				parent.rightChild = current.rightChild;
+			}
+		}
+		else{
+			Node successor = getSuccessor(current);
+			if(current == root){
+				root = successor;
+				successor.leftChild = current.leftChild;
+			}
+			else if(isLeftChild){
+				parent.leftChild = successor;
+			}
+			else{
+				parent.rightChild = successor;
+				successor.leftChild = current.leftChild;
+			}
+		}
+		return;
+	}//end delete method
+	
+	private Node getSuccessor(Node delNode){
+		Node successorParent = delNode; //usa
+		Node successor = delNode;//usa
+		Node current = delNode.rightChild; //wakanda
+		while(current != null){
+			successorParent = successor; //1) usa 2)wakanda
+			successor = current;//1) wakanda 2) vienna
+			current = current.leftChild;//1)vienna 2) null
+		}
+		if(successor != delNode.rightChild){
+			successorParent.leftChild = successor.rightChild;
+			successor.rightChild = delNode.rightChild;
+		}
+		return successor;
+	}
+	
+	public void printInorder(){
+		inOrder(root);
+	}//end printInorder
+	private void inOrder(Node localRoot){
+		if(localRoot != null){
+			inOrder(localRoot.leftChild);
+			localRoot.print();
+			inOrder(localRoot.rightChild);
+		}
 	}
 	public void printPreOrder(){
 		preOrder(root);
@@ -64,5 +165,18 @@ public class BinarySearchTree {
 			preOrder(localRoot.rightChild);
 		}
 	}
+	public void printPostorder(){
+		postOrder(root);
+	}
+	private void postOrder(Node localRoot){
+		if(localRoot != null){
+			postOrder(localRoot.leftChild);
+			postOrder(localRoot.rightChild);
+			localRoot.print();
+		}
+	}
 	
+	public void printBottomCountries(int c){
+		
+	}
 }//end BinarySearchTree class
--- src/Project4.java
+++ src/Project4.java
@@ -1,16 +1,135 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import javax.swing.TransferHandler;
 
 public class Project4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		BinarySearchTree tree = new BinarySearchTree();
-		tree.insert("United States of America", 1.5434);
-		tree.insert("Wakanda", 1.5434);
-		tree.insert("Zimbabwe", 1.5434);
-		tree.insert("Afghanistan", 1.5434);
-		tree.insert("Madagascar", 1.5434);
-		tree.insert("Nigeria", 3.5445);
-		tree.find("Nigeria");
+		System.out.println("COP3530 Project 4");
+		System.out.println("Instructor: Xudong Liu");
+		System.out.println("Student: Riley Tittle\n");
+		System.out.print("Enter the file name: ");
+		Scanner in = new Scanner(System.in);
+		String fileName = in.nextLine();
+
+		FileReader fileReader = null;
+		Scanner fileScanner = null;
+		boolean fileNotFound = false;
+		do{
+			try {
+				fileReader = new FileReader(fileName);
+				fileScanner = new Scanner(fileReader);
+				fileNotFound = false;
+			} catch (FileNotFoundException e) {
+				fileNotFound = true;
+				System.out.print("File not found. Try again: ");
+				fileName = in.nextLine();
+			}
+		}while(fileNotFound);
+
+		BinarySearchTree theTree = new BinarySearchTree();
+		int recordsRead = 0;
+		fileScanner.nextLine(); //discard the header
+		while(fileScanner.hasNextLine()){
+			String currentLine = fileScanner.nextLine();
+			String[] lineData = currentLine.split(",");
+			String countryName = lineData[0];
+			double happinessIndex = Double.parseDouble(lineData[5]);
+			theTree.insert(countryName, happinessIndex);
+			recordsRead++;
+		}
+
+		System.out.println("There were " + recordsRead + " records read to build a binary search tree\n");
+
+		boolean programNotFinished = true;
+		
+		/*
+		// test code
+		Scanner in = new Scanner(System.in);
+		boolean programNotFinished = true;
+		BinarySearchTree theTree = new BinarySearchTree();
+		theTree.insert("United States of America", 1.5434);
+		theTree.insert("Wakanda", 1.5434);
+		theTree.insert("Zimbabwe", 1.5434);
+		theTree.insert("Afghanistan", 1.5434);
+		theTree.insert("Vienna", 3.254);
+		theTree.insert("Madagascar", 1.5434);
+		theTree.insert("Nigeria", 3.5445);
+		theTree.printInorder();
+		*/
+		do{
+			System.out.println("1) Print tree inorder");
+			System.out.println("2) Print tree preorder");
+			System.out.println("3) Print tree postorder");
+			System.out.println("4) Insert a country with name and happiness");
+			System.out.println("5) Delete a country for a given name");
+			System.out.println("6) Search and print a country and its path for a given name");
+			System.out.println("7) Print bottom countries regarding happiness");
+			System.out.println("8) Print top countries regarding happiness");
+			System.out.println("9) Exit");
+			System.out.print("Enter your choice: ");
+			String menuChoice = "";
+			boolean invalidChoice = false;
+			do {
+				try {
+					menuChoice = in.nextLine();
+					if(Integer.parseInt(menuChoice) > 9 || Integer.parseInt(menuChoice) < 1) {
+						System.out.print("Invalid choice, enter 1-9: ");
+						invalidChoice = true;
+					}
+					else {
+						invalidChoice = false;
+					}
+				}
+				catch(NumberFormatException e) {
+					System.out.print("Invalid Choice, enter 1-9: ");
+					invalidChoice = true;
+				}
+			}while(invalidChoice);
+
+			switch(Integer.parseInt(menuChoice)){
+			case 1:
+				theTree.printInorder();
+				break;
+			case 2:
+				theTree.printPreOrder();
+				break;
+			case 3:
+				theTree.printPostorder();
+				break;
+			case 4:
+				System.out.print("Enter country name: ");
+				String nameToInsert = in.nextLine();
+				System.out.print("Enter country happiness: ");
+				double happiness = Double.parseDouble(in.nextLine());
+				theTree.insert(nameToInsert, happiness);
+				break;
+			case 5:
+				System.out.print("Enter country name: ");
+				String nameToDelete = in.nextLine();
+				theTree.delete(nameToDelete);
+				break;
+			case 6:
+				System.out.print("Enter country name: ");
+				String nameToFind = in.nextLine();
+				theTree.find(nameToFind);
+				break;
+			case 7:
+				//write method 
+				break;
+			case 8:
+				//write method
+				break;
+			case 9:
+				programNotFinished = false;
+				break;
+			}
+		}while(programNotFinished);
+		System.out.println("\nHave a good day!");
+
 	}
 
 }
